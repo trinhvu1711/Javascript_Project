29da14cdb26d1c4ac167ebcfbd992e508e2fb2e6
Trịnh Long Vũ <81180330+trinhvu1711@users.noreply.github.com>
2023-09-12 08:31:38 +0700

    complete game

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -9,45 +9,31 @@ const winCondition = [
   [3, 6, 9]
 ];
 
-const board_item = document.querySelectorAll('.board-item')
-
-function resetBoard() {}
-
 const player = (isPlayer1,step, isWin) => {
   const move = (pos) => {
-    const boardPlayer = board_item[pos-1]
-    if (isPlayer1) {
-      boardPlayer.classList.add('isX');
-      boardPlayer.textContent = 'X'
-    }
-    else{
-      boardPlayer.classList.add('isO');
-      boardPlayer.textContent = 'O'
-    }
-    if (step.length !== 3) {
-      step.push(pos)
-    }
-    else {
-      winCondition.forEach(element => {
-        if(arraysAreEqual(element, step))
+    step.push(pos)
+    winCondition.forEach(element => {
+      if(arraysAreEqual(step, element)){
         isWin = true;
-      });
-      isWin = false;
-    }
+        // console.log(isWin);
+      }
+    });
+    return isWin;
   }
-  return {step, isWin, move}
+  const getIsWin = () => {
+    return move() // Call the move function to calculate isWin
+  };
+  return {step, getIsWin, move}
 }
 
 function arraysAreEqual(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
+  let index = 0;
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
+    if (arr1[i] === arr2[index]) {
+      index++;
     }
+    if (index === 3) return true
   }
 
-  return true;
+  return false;
 }
